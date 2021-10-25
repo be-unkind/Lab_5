49a6f59e38815fae37c0af9f902ea2603011d0d2
Nastunia <gavrylivnastya@gmail.com>
2021-10-25 16:25:56 +0300

    update

--- src/main/java/flowerstore/Store.java
+++ src/main/java/flowerstore/Store.java
@@ -1,8 +1,37 @@
 package flowerstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Store {
 
-    private void search(){
+    List<Flower> flowers = new ArrayList<Flower>();
+    List<FlowerPack> flowerPacks = new ArrayList<FlowerPack>();
+    List<FlowerBucket> flowerBuckets = new ArrayList<FlowerBucket>();
 
+    public void addFlower(Flower flower){
+        flowers.add(flower);
     }
+
+    public void addPack(FlowerPack flowerPack){
+        flowerPacks.add(flowerPack);
+    }
+
+    public void addBucket(FlowerBucket flowerBucket){
+        flowerBuckets.add(flowerBucket);
+    }
+
+    public boolean containsFlower(Flower flower){
+        return flowers.contains(flower);
+    }
+
+    public boolean containsPack(FlowerPack flowerPack){
+        return flowerPacks.contains(flowerPack);
+    }
+
+    public boolean containsBucket(FlowerBucket flowerBucket){
+        return flowerPacks.contains(flowerBucket);
+    }
+
 }
---
+++ src/test/java/flowerstore/StoreTest.java
@@ -0,0 +1,16 @@
+package flowerstore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreTest {
+
+    @Test
+    void addPack() {
+    }
+
+    @Test
+    void containsPack() {
+    }
+}
